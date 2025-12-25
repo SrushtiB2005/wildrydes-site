cd936bbfdbad83d46cceb1098bf919cf17f4e514
SrushtiB2005 <bhosale.srushti2005@gmail.com>
2025-12-25 20:12:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,44 @@
-window._config = {
-    cognito: {
-        userPoolId: 'ap-south-1_aVjkWjMEc', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '623t790ctm0dhc7bcck3enpoi9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+import (
+    "context"
+    "fmt"
+    "html/template"
+    "log"
+    "net/http"
+    "golang.org/x/oauth2"
+    "github.com/coreos/go-oidc"
+    "github.com/golang-jwt/jwt/v4"
+)
+
+type ClaimsPage struct {
+    AccessToken string
+    Claims      jwt.MapClaims
+}
+
+var (
+    clientID     = "623t790ctm0dhc7bcck3enpoi9"
+    clientSecret = "l71fjdreurtu95nkn2ub0o57g1r2k3an1ka6ccka4hjs2n5tkd3"
+    redirectURL  = "https://https://main.d2dls30vbzi549.amplifyapp.com/"
+    issuerURL    = "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_aVjkWjMEc"
+    provider     *oidc.Provider
+    oauth2Config oauth2.Config
+)
+
+func init() {
+    var err error
+    // Initialize OIDC provider
+    provider, err = oidc.NewProvider(context.Background(), issuerURL)
+    if err != nil {
+        log.Fatalf("Failed to create OIDC provider: %v", err)
     }
-};
+
+    // Set up OAuth2 config
+    oauth2Config = oauth2.Config{
+        ClientID:     clientID,
+        ClientSecret: clientSecret,
+        RedirectURL:  redirectURL,
+        Endpoint:     provider.Endpoint(),
+        Scopes:       []string{oidc.ScopeOpenID,"email","openid","phone"},
+    }
+}
 
 
